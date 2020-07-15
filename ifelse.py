e3bd8b8ee02b64ef465787344783bd40d85820a9
saikrishna1426 <68234255+saikrishna1426@users.noreply.github.com>
2020-07-15 07:35:48 -0700

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+num1 = int(input("enter 1st number"))
+num2 = int(input("enter 2nd number"))
+if num1 > num2 :
+    ans= num1 - num2
+    print(ans)
+
+else :
+    ans= num2 - num1
+    print(ans)
+
