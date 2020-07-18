022016ca2f29088213dbb4b14fefc7bbc0e8d11a
saikrishna1426 <68234255+saikrishna1426@users.noreply.github.com>
2020-07-18 00:07:40 -0700

    Add files via upload
    if else using logical operators condition

---
+++ advanced python assignments/assignment3.py
@@ -0,0 +1,12 @@
+uname = 'saikrishna'
+pword = 'sai12345'
+
+username = input("please enter your username")
+password = input("enter password")
+
+if username == uname and password == pword :
+    print(f"hello {uname} welcome here")
+
+else :
+    print("please enter valid password")
+
