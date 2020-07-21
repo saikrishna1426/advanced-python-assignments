bd4d4ba4c00de35fb1ab4fb0d5d179b13dfa69ef
saikrishna1426 <68234255+saikrishna1426@users.noreply.github.com>
2020-07-21 08:34:07 -0700

    Add files via upload
    to count how many times..the loop is executed

---
+++ advanced python assignments/asgmt4(countloop).py
@@ -0,0 +1,9 @@
+# to calculate how many times the code (loop) is running,
+count = 0
+num = 50616
+
+while num < 151515100 :
+    print("krishna")
+    count = count +1
+    num += 15194
+print("number of times the loop executed is", count)
